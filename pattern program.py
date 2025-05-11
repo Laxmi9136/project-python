84a0221b1ee41251bbb6fe50f7ebd2def8f537e3
Laxmi <singhsanjay95080@gmail.com>
2025-05-11 14:50:40 +0530

    Add files via upload

---
+++ pattern program.py
@@ -0,0 +1,155 @@
+"""
+for i in range(1,11):
+    for a in  range(1,i+1):
+        print(a,end="")
+    print(end=" ")
+    for b in range(11-a,0,-1):
+        print(b,end="")
+    print()"""
+
+"""
+n=5
+for i in range(1,n+1):
+    for j in range(1,2*n):
+        if j==n-i+1 or j==n+i-1 or i==n:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()"""
+"""
+ch="L"
+if(ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
+    print("Vowel",ch)
+else:
+    print("Constant")
+"""
+
+"""ch="L"
+if(ch in "AEIOUaeiou"):
+    print("vowel",ch)
+else:
+    print("constant")"""
+
+
+"""amount=float(input("Enter amount:"))
+if (amount>5000):
+    print("bill:",amount*80/100)
+elif(amount>3000):
+    print("bill:",amount*90/100)
+else:
+    print("bill:",amount*95/100)"""
+
+"""
+a=2
+b=3
+c=10
+sum=0
+if ((a+b)>c):
+    sum=b+a
+    print(sum,"triangle")
+else:
+    print("not triangle")"""
+
+"""bs=2000
+hra=bs*20/100
+da=bs*30/100
+gs=bs+hra+da
+print("Gross salery",gs)"""
+
+for i  in range(1,5):
+    for a in range(4-i):
+        print(" ",end=" ")
+    for a in range(1,i*2):
+        print("*",end=" ")
+    print( )
+for i in range(3,0,-1):
+    for a in range(4-i):
+            print(" ",end=" ")
+    for a in range(1,i*2):
+        print("*",end=" ")
+    print( )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+    
+             
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+    
